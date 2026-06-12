afc6c4ebd72587e61dfcdf0b49966103df63efd5
Andre Virgilli <andrevirgilio@gmail.com>
2026-06-12 16:01:31 -0300

    Add user data extraction from API

--- extract_users.py
+++ extract_users.py
@@ -1,1 +1,23 @@
-print("API Data Ingestion Project")
+import requests
+import pandas as pd
+
+response = requests.get("https://jsonplaceholder.typicode.com/users")
+
+users = response.json()
+
+df = pd.DataFrame(users)
+
+df = df[
+    [
+        "id",
+        "name",
+        "username",
+        "email",
+        "phone",
+        "website"
+    ]
+]
+
+df.to_csv("users.csv", index=False)
+
+print("CSV file generated successfully")
