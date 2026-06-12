828dfa74a20df31ac80623bc92d4b9c0a7088b7c
Andre Virgilli <andrevirgilio@gmail.com>
2026-06-12 16:16:13 -0300

    Refactor ETL process into functions

--- extract_users.py
+++ extract_users.py
@@ -1,23 +1,41 @@
 import requests
 import pandas as pd
 
-response = requests.get("https://jsonplaceholder.typicode.com/users")
 
-users = response.json()
+def extract_users():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
 
-df = pd.DataFrame(users)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Request failed. Status code: {response.status_code}")
+        return None
 
-df = df[
-    [
-        "id",
-        "name",
-        "username",
-        "email",
-        "phone",
-        "website"
+def transform_users(users):
+    df = pd.DataFrame(users)
+
+    df = df[
+        [
+            "id",
+            "name",
+            "username",
+            "email",
+            "phone",
+            "website"
+        ]
     ]
-]
+    return df
 
-df.to_csv("users.csv", index=False)
+def load_users(df):
+    df.to_csv("users.csv", index=False)
 
-print("CSV file generated successfully")
+    print("CSV file generated successfully")
+
+users = extract_users()
+
+if users:
+    df = transform_users(users)
+
+    print(f"Number of users retrieved: {len(df)}")
+
+    load_users(df)
